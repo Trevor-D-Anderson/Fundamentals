d762a255ff29997c25dc2a48d1ffe38b7f08fddd
Trevor Anderson <tbone101@gmail.com>
2021-09-28 22:20:24 -0700

    3 was way easier than 2

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -33,10 +33,19 @@ def iterateDictionary(variable):
         print('first_name - {}, last_name - {}'.format(variable[x]['first_name'], variable[x]['last_name']))
 
 
-iterateDictionary(students) 
+# iterateDictionary(students) 
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
 # first_name - Michael, last_name - Jordan
 # first_name - John, last_name - Rosales
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
+
+# 3. Get Values From a List of Dictionaries
+def iterateDictionary2(key_name, some_list):
+    for x in range(len(some_list)):
+        print(some_list[x][key_name])
+
+
+# iterateDictionary2('first_name', students)
+# iterateDictionary2('last_name', students)
