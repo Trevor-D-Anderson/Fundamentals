906ad45e05aa1b0adcea65238dc6e7c4af9edab9
Trevor Anderson <tbone101@gmail.com>
2021-09-28 22:10:50 -0700

    I hated problem 2

--- functions_intermediate1.py
+++ functions_intermediate1.py
@@ -22,3 +22,21 @@ changeX(15)
 # print(z)
 
 # 2. Iterate Trough a List of Dictionaries
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(variable):
+    for x in range(len(variable)):
+        print('first_name - {}, last_name - {}'.format(variable[x]['first_name'], variable[x]['last_name']))
+
+
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
