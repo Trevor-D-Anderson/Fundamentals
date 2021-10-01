8bfc4187a5cd7a7d4016390a69ba34fd2aa188fb
Trevor Anderson <tbone101@gmail.com>
2021-09-30 20:56:56 -0700

    Added Chaining

--- User/User.py
+++ User/User.py
@@ -8,45 +8,37 @@ class User:
     def depositChecking(self, amount):
         self.checkingBalance += amount
         print(f"You have deposited ${amount}")
+        return self
 # Withdrawl
     def withdrawlChecking(self, amount):
         self.checkingBalance -= amount
         print(f"you have withdrawn ${amount}")
+        return self
 # Transfer checking to Savings
     def transferCheckingToSavings(self, amount):
         self.checkingBalance -= amount
         self.savingsBalance += amount
         print(f"{self.firstName} has ${self.checkingBalance} in Checking and ${self.savingsBalance} in Savings.")
+        return self
 # Display Balance
     def displayBalance(self):
         print(f"You have in Checking: ${self.checkingBalance}")
         print(f"You have in Savings: ${self.savingsBalance}")
+        return self
 # User Transfer
     def userTransfer(self, user, amount):
         self.checkingBalance -= amount
         user.checkingBalance += amount
         print(f"{self.firstName} has transferred ${amount} to {user.firstName}")
+        return self
 
 trevor = User("Trevor", "Anderson")
 hannah = User("Hannah", "Anderson")
 joe = User("Joe", "Young")
 
-trevor.depositChecking(500)
-trevor.depositChecking(500)
-trevor.depositChecking(1500)
-trevor.withdrawlChecking(50)
-trevor.displayBalance()
-hannah.depositChecking(150)
-hannah.depositChecking(150)
-hannah.withdrawlChecking(25)
-hannah.withdrawlChecking(75)
-hannah.displayBalance()
-trevor.transferCheckingToSavings(200)
-trevor.userTransfer(hannah, 100)
-joe.depositChecking(800)
-joe.withdrawlChecking(75)
-joe.withdrawlChecking(75)
-joe.withdrawlChecking(75)
-joe.displayBalance()
+trevor.depositChecking(500).depositChecking(500).depositChecking(1500).withdrawlChecking(50).displayBalance()
+hannah.depositChecking(150).depositChecking(150).withdrawlChecking(25).withdrawlChecking(75).displayBalance()
+trevor.transferCheckingToSavings(200).userTransfer(hannah, 100)
+joe.depositChecking(800).withdrawlChecking(75).withdrawlChecking(75).withdrawlChecking(75).displayBalance()
 trevor.displayBalance()
 hannah.displayBalance()
