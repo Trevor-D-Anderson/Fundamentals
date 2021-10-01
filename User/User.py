5250c4e21949b9e9d5a4f1e1d9a77e3d792856a5
Trevor Anderson <tbone101@gmail.com>
2021-09-30 20:50:20 -0700

    Fixed...

--- User/User.py
+++ User/User.py
@@ -8,50 +8,6 @@ class User:
     def depositChecking(self, amount):
         self.checkingBalance += amount
         print(f"You have deposited ${amount}")
-        print(f"{self.firstName} has a checking balance of: ${self.checkingBalance}")
-# Withdrawl
-    def withdrawlChecking(self, amount):
-        self.checkingBalance -= amount
-        print(f"you have withdrawn ${amount}")
-        print(f"{self.firstName} has a checking balance of: ${self.checkingBalance}")
-# Transfer checking to Savings
-    def transferCheckingToSavings(self, amount):
-        self.checkingBalance -= amount
-        self.savingsBalance += amount
-        print(f"{self.firstName} has ${self.checkingBalance} in Checking and ${self.savingsBalance} in Savings.")
-# Display Balance
-    def displayBalance(self):
-        print(f"You have in Checking: ${self.checkingBalance}")
-        print(f"You have in Savings: ${self.savingsBalance}")
-# User Transfer
-    def userTransfer(self, user, amount):
-        self.checkingBalance -= amount
-        user.checkingBalance += amount
-        print(f"{self.firstName} has transferred ${amount} to {user.firstName}")
-
-trevor = User("Trevor", "Anderson")
-hannah = User("Hannah", "Anderson")
-joe = User("Joe", "Young")
-
-trevor.depositChecking(500)
-trevor.depositChecking(500)
-trevor.depositChecking(1500)
-trevor.withdrawlChecking(50)
-hannah.depositChecking(150)
-trevor.transferCheckingToSavings(200)
-trevor.userTransfer(hannah, 100)
-trevor.displayBalance()
-hannah.displayBalance()
-class User:
-    def __init__(self, firstName, lastName):
-        self.firstName = firstName
-        self.lastName = lastName
-        self.checkingBalance = 0
-        self.savingsBalance = 0
-# Deposit
-    def depositChecking(self, amount):
-        self.checkingBalance += amount
-        print(f"You have deposited ${amount}")
 # Withdrawl
     def withdrawlChecking(self, amount):
         self.checkingBalance -= amount
